1e86bfcf0412ff0548a5e5272de74f613cabfc0b
FuQian WenMin <1.heart.2.points@gmail.com>
2017-08-31 08:43:32 +0800

    unpacker

---
+++ src/Core/DataUnpacker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CnDream.Core
+{
+    public class DataUnpacker : IDataUnpacker
+    {
+        public bool UnpackData
+        (
+            ArraySegment<byte> output,
+            out (int bytesWritten, int bytesRead, int? pairId, int? serialId, int? payloadSize) description,
+            ArraySegment<byte> input
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
