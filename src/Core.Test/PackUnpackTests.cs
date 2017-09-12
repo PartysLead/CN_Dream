e7736afca8e97cd52398e558818c3159301ad178
FuQian WenMin <1.heart.2.points@gmail.com>
2017-09-12 18:27:09 +0800

    tests

---
+++ src/Core.Test/PackUnpackTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CnDream.Core.Test
+{
+    public class PackUnpackTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
