72b6c4648cc7355710aa76751a30e0ee1fe0433c
FuQian WenMin <1.heart.2.points@gmail.com>
2017-11-03 14:49:11 +0800

    Add sliced test.

--- src/Core.Test/UnpackerTests.cs
+++ src/Core.Test/UnpackerTests.cs
@@ -39,5 +39,50 @@ namespace CnDream.Core.Test
             Assert.Equal(2, serialId);
             Assert.Equal(4, payloadSize);
         }
+
+        [Fact]
+        public void SmallPayloadSliced()
+        {
+            var unpacker = new DataUnpacker(new IdentityTransformer());
+            var input = new byte[]
+            {
+                1, 2, 3, 2,
+                1, 0, 0, 0,
+                2, 0, 0, 0,
+                4, 0, 0, 0,
+
+                1, 2, 3, 4,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+            };
+            var output = new byte[16];
+
+            var unpacked = false;
+            int bytesWritten, bytesRead, pairId, serialId, payloadSize;
+            for ( int i = 0; i < 4; i++ )
+            {
+                unpacked = unpacker.UnpackData(
+                    new ArraySegment<byte>(output),
+                    out bytesWritten, out bytesRead,
+                    out pairId, out serialId, out payloadSize, new ArraySegment<byte>(input, i * 4, 4));
+
+                Assert.Equal(false, unpacked);
+            }
+
+            unpacked = unpacker.UnpackData(
+                new ArraySegment<byte>(output),
+                out bytesWritten, out bytesRead,
+                out pairId, out serialId, out payloadSize, new ArraySegment<byte>(input, 4 * 4, 16));
+
+            Assert.Equal(true, unpacked);
+
+            Assert.Equal(payloadSize, bytesWritten);
+            Assert.Equal(16, bytesRead);
+
+            Assert.Equal(1, pairId);
+            Assert.Equal(2, serialId);
+            Assert.Equal(4, payloadSize);
+        }
     }
 }
